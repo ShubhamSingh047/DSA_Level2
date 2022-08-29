b95475ea863c858c738c397c55ee40b158695bc5
ShubhamSingh047 <shubham.shubham.singh13@gmail.com>
2022-08-30 02:26:35 +0530

    SubSequence_recursion

--- Subsequence_Print.java
+++ Subsequence_Print.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
+
 public class Subsequence_Print {
 
+    static ArrayList Sol(String str){
+        if(str.length()==0){
+            ArrayList<String> failcase= new ArrayList<>();
+            failcase.add("");
+            return failcase;
+        }
+        char firstChar =  str.charAt(0);
+        String restStr = str.substring(1);
+
+        ArrayList<String> rres = Sol(restStr);
+        ArrayList<String> myres = new ArrayList<>();
+        for(String s:rres){
+            myres.add(""+s);
+            myres.add(firstChar+s);
+        }
+        return myres;
+    }
     public static void main(String[] args) {
-        System.out.println("pending");
+        ArrayList<String> res=Sol("abc");
+        System.out.println(res);
     }
 }
